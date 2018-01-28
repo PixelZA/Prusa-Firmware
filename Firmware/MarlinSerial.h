517324328f2687a724f9bd45bd280464772f267a
3d-gussner <3d.gussner@gmail.com>
2018-01-28 15:16:31 +0100

    2nd fix

--- Firmware/MarlinSerial.h
+++ Firmware/MarlinSerial.h
@@ -156,7 +156,7 @@ class MarlinSerial //: public Stream
                 if (UCSR1A & (1<<FE1)) {
                     // Characters received with the framing errors will be ignored.
                     // The temporary variable "c" was made volatile, so the compiler does not optimize this out.
-                    volatile unsigned char c = UDR1;
+                    (void)(*(char *)UDR1);
                 } else {
                     unsigned char c  =  UDR1;
                     int i = (unsigned int)(rx_buffer.head + 1) % RX_BUFFER_SIZE;
